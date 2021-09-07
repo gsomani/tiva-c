0de9696e89164a67d1c9ca4af9c8a8c3547adf2d
Gaurav Somani <gauravsomani@iisc.ac.in>
2021-09-08 03:11:11 +0530

    remove generated files

--- boards/ek-tm4c123gxl/freertos_demo/freertos_demo.c
+++ boards/ek-tm4c123gxl/freertos_demo/freertos_demo.c
@@ -102,8 +102,8 @@ __error__(char *pcFilename, uint32_t ui32Line)
 // This hook is called by FreeRTOS when an stack overflow error is detected.
 //
 //*****************************************************************************
-void
-vApplicationStackOverflowHook(xTaskHandle *pxTask, char *pcTaskName)
+void 
+vApplicationStackOverflowHook( TaskHandle_t xTask, char * pcTaskName )
 {
     //
     // This function can not return, so loop forever.  Interrupts are disabled
